dc972136f2d18a2cb45ebde981f7105bd1e51bcd
Vitaly-Nik <nikitalik93@mail.ru>
2023-05-08 19:36:38 +0300

    fix: add copydir function in 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,28 +4,31 @@ const path = require("path");
 const folderPath = path.join(__dirname, "files");
 const folderPathCopy = path.join(__dirname, "files-copy");
 
-fs.access(folderPathCopy, (err) => {
-  if (err) {
-    fs.mkdir(folderPathCopy, (err) => {
-      if (err) {
-        throw err;
-      }
-    });
-  }
-
-  fs.readdir(folderPath, (err, files) => {
+function copyDir() {
+  fs.access(folderPathCopy, (err) => {
     if (err) {
-      throw err;
-    }
-    for (let file of files) {
-      let filePath = path.join(folderPath, file);
-      let filePathCopy = path.join(folderPathCopy, file);
-
-      fs.copyFile(filePath, filePathCopy, (err) => {
+      fs.mkdir(folderPathCopy, (err) => {
         if (err) {
           throw err;
         }
       });
     }
+
+    fs.readdir(folderPath, (err, files) => {
+      if (err) {
+        throw err;
+      }
+      for (let file of files) {
+        let filePath = path.join(folderPath, file);
+        let filePathCopy = path.join(folderPathCopy, file);
+
+        fs.copyFile(filePath, filePathCopy, (err) => {
+          if (err) {
+            throw err;
+          }
+        });
+      }
+    });
   });
-});
+}
+copyDir();
